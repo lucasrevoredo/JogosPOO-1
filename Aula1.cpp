56ad139e1c79f7e4a2325bfc822de4d8b7afaf37
Hildelitan Pereira <hildelitanpereira@outlook.com>
2016-05-31 21:29:29 -0300

    Atualização 1
    Nova atualização.
    Criando um projeto no GitHub.
    Adicionando meu primeiro arquivo.

--- Aula1.cpp
+++ Aula1.cpp
@@ -1,9 +1,12 @@
 //Meu primeiro código em C++.
+
+//Incluindo na biblioteca.
 #include <iostream>
 using namespace std;
 
 int main(int argc, char* args[])
 {
+	//Imprimindo mensagem na tela.
 	cout << "Hello World" << endl;
 
 	return 0;
